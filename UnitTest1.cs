0fb8dc5ac9344164813cb05d02df6b97a0d5e9cb
Flavio <conectef@gmail.com>
2021-01-21 20:45:12 -0300

    git status

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dotnet_vaquinha_tests_master
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
